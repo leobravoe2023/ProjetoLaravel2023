99fc541b51180df127905d90b20b507f64002d25
leobravoe2023-2 <leonardo.estacio.gdrive2023-2@gmail.com>
2023-06-06 11:43:22 -0300

    final da aula 50

--- public/js/pedidoUsuario.js
+++ public/js/pedidoUsuario.js
@@ -1,5 +1,15 @@
 // executa o JS depois de todo HTML ser carregado
 $(document).ready( function () {
+
+    function groupBy(arr, property){
+        return arr.reduce( function (anterior, atual){
+            if(!anterior[atual[property]]){
+                anterior[atual[property]] = [];
+            }
+            anterior[atual[property]].push(atual);
+            return anterior;
+        }, [] );
+    }
     
     const selectFiltroTipo = $("#id-select-filtro-tipo");
     
@@ -23,8 +33,49 @@ $(document).ready( function () {
                 // Apago qualquer informação dentro do local selecionado
                 divProduto.html("");
                 // Imprimo os dados recebidos
-                response.return.forEach(produto => {
-                    divProduto.append(`<p>${produto.id} - ${produto.nome}</p>`);
+                produtos_group = groupBy(response.return, "Tipo_Produtos_id");
+                produtos_group.forEach(produtos_tipo => {
+                    // Imprimir inforções do tipo
+                    divProduto.append(`
+                        <div class="my-4 border border-dark">
+                            <div class="m-4">
+                                <h4 class="d-inline">${produtos_tipo[0].descricao}</h4>
+                                <select class="float-end">
+                                    <option value="">Ordem do sistema</option>
+                                    <option value="">Menor para maior</option>
+                                    <option value="">Maior para menor</option>
+                                </select>
+                            </div>
+                            <div class="my-4 produto">
+                            </div>
+                        </div>
+                    `);
+                    produtos_tipo.forEach(produto => {
+                        // Imprimir as informação da variável produto
+                        $(".my-4.produto:last").append(`
+                            <div class="row m-3 border border-dark">
+                                <div class="col-md-3 my-auto">
+                                    <img class="w-100 h-100" src="${produto.urlImage}">
+                                </div>
+                                <div class="col-md-6 my-auto">
+                                    <h5>${produto.nome}</h5>
+                                    <h6>Ingredientes:</h6>
+                                    <p>${produto.ingredientes}</p>
+                                </div>
+                                <div class="col-md-3 my-auto">
+                                    <div class="my-3">
+                                        <input type="text" class="form-control" value="R$ ${produto.preco}" readonly>
+                                    </div>
+                                    <div class="my-3">
+                                        <input type="number" class="form-control" id="id-quant-produto-${produto.id}" value="1">
+                                    </div>
+                                    <div class="my-3">
+                                        <a class="btn btn-primary w-100 btn-add-produto" value="${produto.id}" value-tipo="${produto.Tipo_Produtos_id}">Adicionar Produto</a>
+                                    </div>
+                                </div>
+                            </div>
+                        `);
+                    });
                 });
             },
             error: function(error){
@@ -33,5 +84,7 @@ $(document).ready( function () {
             }
         });
     }
+
+    updateProdutos();
 
 });
